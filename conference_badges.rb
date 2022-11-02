c89243c242f48628725a17fdc56f8e11a80e8d7c
Alex Ju <aju217@gmail.com>
2022-11-02 14:52:43 -0400

    commit tree 2cae14eb978cc020cd26a1879acb8a84ec909da8

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+require "pry"
+def badge_maker(str)
+    "Hello, my name is #{str}."
+end
+
+def batch_badge_creator(name_array)
+    name_array.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms(name_array)
+    name_array.map do |name|
+        "Hello, #{name}! You'll be assigned to room #{name_array.find_index(name) + 1}!"
+    end
+end
+
+def printer(name_array)
+    batch_badge_creator(name_array).each do |name|
+        puts name
+    end
+
+    assign_rooms(name_array).each do |name|
+        puts name
+    end
+end
+
+binding.pry
